6d7466937aee2dabba6ebf8ad4aac4200fff45a3
Nutifafa Afi Attor <attorfafa@gmail.com>
2021-11-12 17:25:03 +0000

    redesigned team page

--- js/team.js
+++ js/team.js
@@ -7,3 +7,18 @@ menuBtn.addEventListener("click", () => {
     navigation.classList.toggle("active")
 
 })
+
+
+var swiper = new Swiper('.blog-slider', {
+  spaceBetween: 30,
+  effect: 'fade',
+  loop: true,
+  mousewheel: {
+    invert: false,
+  },
+  // autoHeight: true,
+  pagination: {
+    el: '.blog-slider__pagination',
+    clickable: true,
+  }
+});
